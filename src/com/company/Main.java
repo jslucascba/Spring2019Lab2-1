e615d8c9d9dc6c52b122b0d2da195209dcfc923f
Jeff Lucas <jslucas1@msn.com>
2019-01-24 16:09:49 -0600

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+//Add import statement to allow you to read data from the keyboard below this line
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Add code to declare and initialize constants
+
+
+        //Add code to create scanner object here
+
+
+        //Add code to request height here
+
+
+        //Add code to read height from keyboard and assign to an integer variable called height that you declare
+
+
+        //Add code to request steps from user
+
+
+        //Add code to read steps from keyboard and assign to an integer variable you declare called steps
+
+
+        //Add code to calculate the stride length and assign it to the stideLength variable that you declare
+
+
+        //Add code to convert strideLength to feet
+
+
+        //Add code to calculate feet walked
+
+
+        //Add code to declare and calculate milesWalked
+
+
+        //Add code to display number of miles walked
+
+
+    }
+}
